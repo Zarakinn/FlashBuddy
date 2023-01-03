a80fb972a1a122fbc10d488de324b700e0888e2d
Nicolas Frache <frache.nicolas@gmail.com>
2023-01-03 17:05:51 +0100

    [NEW] Header et switch entre les onglets + refacto

--- flashcards/src/main/java/tn/flashcards/controller/MainController.java
+++ flashcards/src/main/java/tn/flashcards/controller/MainController.java
@@ -2,13 +2,11 @@ package tn.flashcards.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -18,73 +16,50 @@ public class MainController implements Initializable, Observateur {
     @FXML
     StackPane ViewHolder;
 
-    Pane Edit;
-    Pane Parameter;
-    Pane Stats;
-    Pane Training;
+    @FXML
+    RadioButton b1, b2, b3, b4;
+
+    @FXML
+    Pane edit, parameter, stats, training;
 
     List<Pane> onglets;
 
-
-    public MainController(Pane Edit, Pane Parameter, Pane Stats, Pane Training)
-    {
-        this.Edit = Edit;
-        this.Parameter = Parameter;
-        this.Stats = Stats;
-        this.Training = Training;
-
-        onglets = Arrays.asList(Edit,Parameter,Stats,Training);
+    public MainController() {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle)
-    {
-        for (Pane p:onglets)
-        {
-            ViewHolder.getChildren().add(p);
-            p.setVisible(false);
-        }
-        setView(Training);
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Arrays.asList(b1, b2, b3, b4).forEach(radioButton -> radioButton.getStyleClass().remove("radio-button"));
+        onglets = Arrays.asList(edit, parameter, stats, training);
+        setViewTraining();
     }
 
     @FXML
-    public void setViewEdit()
-    {
-        setView(Edit);
+    public void setViewEdit() {
+        setView(edit);
     }
 
     @FXML
-    public void setViewParameter()
-    {
-        setView(Parameter);
+    public void setViewParameter() {
+        setView(parameter);
     }
 
     @FXML
-    public void setViewStats()
-    {
-        setView(Stats);
+    public void setViewStats() {
+        setView(stats);
     }
 
     @FXML
-    public void setViewTraining()
-    {
-        setView(Training);
+    public void setViewTraining() {
+        setView(training);
     }
 
 
-    public void setView(Pane pane)
-    {
-        for (Pane p: onglets)
-        {
-            if (p == pane)
-                p.setVisible(true);
-            else
-                p.setVisible(false);
+    public void setView(Pane pane) {
+        for (Pane p : onglets) {
+            p.setVisible(p == pane);
         }
     }
-
-
-
 
     @Override
     public void reagir() {
