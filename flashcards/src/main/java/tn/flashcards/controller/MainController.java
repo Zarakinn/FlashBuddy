88498bc00f3a71a54cfd4ec4706d2079bcfef7f7
Valentin <valentin.chanel@telecomnancy.eu>
2023-01-03 15:06:27 +0100

    Création des classes java

---
+++ flashcards/src/main/java/tn/flashcards/controller/MainController.java
@@ -0,0 +1,34 @@
+package tn.flashcards.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable, Observateur {
+
+    @FXML
+    Pane ViewHolder;
+
+
+    public MainController()
+    {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
+
+
+    @Override
+    public void reagir() {
+
+    }
+}
---
+++ flashcards/src/main/java/tn/flashcards/controller/StatsController.java
@@ -0,0 +1,26 @@
+package tn.flashcards.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StatsController implements Initializable, Observateur {
+
+    public StatsController()
+    {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @Override
+    public void reagir() {
+
+    }
+}
