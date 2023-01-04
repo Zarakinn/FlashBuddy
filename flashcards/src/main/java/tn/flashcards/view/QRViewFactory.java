f89fd6fe5de159dd9f783550a6f786676ed3445f
Cyrielle Lacrampe--Diter <cyrielle.lacrampe--diter@telecomnancy.eu>
2023-01-04 14:35:22 +0100

    [ADD] package view

---
+++ flashcards/src/main/java/tn/flashcards/view/QRViewFactory.java
@@ -0,0 +1,17 @@
+package tn.flashcards.view;
+
+import tn.flashcards.model.pile.QRType;
+import tn.flashcards.model.pile.QuestionReponse;
+
+public class QRViewFactory {
+    public static QRView createQRView(QuestionReponse qr) {
+        if (qr.getType() == QRType.TEXT) {
+            return new QRTextView(qr.getContent()) ;
+        }
+        else if (qr.getType() == QRType.IMAGE) {
+            return new QRImageView(qr.getContent()) ;
+        }
+
+        return null ;
+    }
+}
