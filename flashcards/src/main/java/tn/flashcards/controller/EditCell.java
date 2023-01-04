08de61356a44c0a68e33da24b5e2d3ca11418f16
Valentin <valentin.chanel@telecomnancy.eu>
2023-01-04 14:32:29 +0100

    [EDIT] modification d'une pile

--- flashcards/src/main/java/tn/flashcards/controller/EditCell.java
+++ flashcards/src/main/java/tn/flashcards/controller/EditCell.java
@@ -1,17 +1,159 @@
 package tn.flashcards.controller;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import tn.flashcards.model.pile.Card;
+import tn.flashcards.model.pile.QRType;
+
+import java.util.Arrays;
 
 public class EditCell extends ListCell<Card> {
 
-    HBox view = new HBox();
+    HBox view;
+    ComboBox<QRType> QuestionType; // TODO - à remplacer par l'énum type de carte
+
+    ComboBox<QRType> AnswerType;
+
+    Label l,l2;
+
+    Button editQuestion, editAnswer;
 
     public EditCell()
     {
+
+    }
+
+    public void createComboBox(Card c)
+    {
+        QuestionType = new ComboBox<QRType>();
+        ObservableList<QRType> oS = FXCollections.observableList(Arrays.asList(QRType.TEXT,QRType.IMAGE));
+        QuestionType.setItems(oS);
+        QuestionType.getSelectionModel().selectFirst();
+
+        QuestionType.setOnAction(
+                event -> {
+                    switch (QuestionType.getValue())
+                    {
+                        case TEXT:
+                            System.out.println("La question de " + String.valueOf(c.getId()) + " devient un Text");
+                            break;
+                        case IMAGE:
+                            System.out.println("La question de " + String.valueOf(c.getId()) + " devient une Image");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+        );
+
+        AnswerType = new ComboBox<QRType>();
+        AnswerType.setItems(oS);
+        AnswerType.getSelectionModel().selectFirst();
+        AnswerType.setOnAction(event -> {
+            switch (QuestionType.getValue())
+            {
+                case TEXT:
+                    System.out.println("La réponse de " + String.valueOf(c.getId()) + " devient un Text");
+                    c.getQuestion().setType(QRType.TEXT);
+                    break;
+                case IMAGE:
+                    c.getQuestion().setType(QRType.IMAGE);
+                    System.out.println("La réponse de " + String.valueOf(c.getId()) + " devient une Image");
+                    break;
+                default:
+                    break;
+            }
+
+        });
+
+    }
+
+    public void createButton(Card c)
+    {
+        editQuestion = new Button("Edit");
+
+        editQuestion.setOnAction(event -> {
+            switch (QuestionType.getValue())
+            {
+                case TEXT:
+                    GridPane form = new GridPane();
+
+                    // Add form elements
+                    TextField nameField = new TextField();
+                    Button submitButton = new Button("Submit");
+                    Label responseLabel = new Label();
+
+                    form.add(new Label("Name:"), 0, 0);
+                    form.add(nameField, 1, 0);
+                    form.add(submitButton, 1, 1);
+                    form.add(responseLabel, 1, 2);
+
+                    Stage formStage = new Stage();
+                    formStage.setTitle("Edit Form");
+
+                    // Set event handler for submit button
+                    submitButton.setOnAction( submited-> {
+                        String value = nameField.getText();
+                        c.getQuestion().setContent(value);
+                        formStage.close();
+                    });
+
+                    Scene formScene = new Scene(form, 300, 200);
+                    formStage.setScene(formScene);
+                    formStage.show();
+                    break;
+                case IMAGE:
+                    System.out.println("Créer file chooser, modifie c sachant que c'est une image");
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        editAnswer = new Button("Edit");
+        editAnswer.setOnAction(event -> {
+            switch (AnswerType.getValue())
+            {
+                case TEXT:
+                    GridPane form = new GridPane();
+
+                    // Add form elements
+                    TextField nameField = new TextField();
+                    Button submitButton = new Button("Submit");
+                    Label responseLabel = new Label();
+
+                    form.add(new Label("Name:"), 0, 0);
+                    form.add(nameField, 1, 0);
+                    form.add(submitButton, 1, 1);
+                    form.add(responseLabel, 1, 2);
+
+                    Stage formStage = new Stage();
+                    formStage.setTitle("Edit Form");
+
+                    // Set event handler for submit button
+                    submitButton.setOnAction( submited-> {
+                        String value = nameField.getText();
+                        c.getReponse().setContent(value);
+                        formStage.close();
+                    });
+
+                    Scene formScene = new Scene(form, 300, 200);
+                    formStage.setScene(formScene);
+                    formStage.show();
+                    break;
+                case IMAGE:
+                    System.out.println("Créer file chooser, modifie c sachant que c'est une image");
+                    break;
+                default:
+                    break;
+            }
+        });
+
 
     }
 
@@ -25,7 +167,13 @@ public class EditCell extends ListCell<Card> {
         }
         else
         {
-            view.getChildren().addAll(new Label(String.valueOf(c.getId())), new Button("Appuie ici"));
+            createComboBox(c);
+            createButton(c);
+            l = new Label(c.getQuestion().getContent());
+            l2 = new Label(c.getReponse().getContent());
+            view = new HBox();
+            view.getChildren().addAll(QuestionType,l, editQuestion, AnswerType, l2, editAnswer);
+            view.setSpacing(10);
             setGraphic(view);
         }
     }
--- flashcards/src/main/java/tn/flashcards/controller/EditPileController.java
+++ flashcards/src/main/java/tn/flashcards/controller/EditPileController.java
@@ -5,11 +5,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.util.Callback;
 import tn.flashcards.model.pile.Card;
 import tn.flashcards.model.pile.Pile;
+import tn.flashcards.model.pile.QRType;
+import tn.flashcards.model.pile.QuestionReponse;
 
 import java.net.URL;
 import java.util.Arrays;
@@ -19,12 +18,17 @@ import java.util.ResourceBundle;
 public class EditPileController implements Initializable, Observateur {
 
     @FXML
+    TextField name;
+
+    @FXML
     ListView<Card> view;
 
+    Pile pile;
 
-    public EditPileController()
+
+    public EditPileController(Pile pile)
     {
-
+        this.pile = pile;
     }
 
     @Override
@@ -32,11 +36,18 @@ public class EditPileController implements Initializable, Observateur {
 
 
         List<Card> cards = Arrays.asList( new Card(26), new Card(38));
+        cards.get(0).setQuestion(new QuestionReponse(QRType.TEXT, "la première question"));
+        cards.get(0).setReponse(new QuestionReponse(QRType.TEXT, "la première réponse"));
+        cards.get(1).setQuestion(new QuestionReponse(QRType.TEXT, "la deuxième question"));
+        cards.get(1).setReponse(new QuestionReponse(QRType.TEXT, "la deuxième réponse"));
+
 
         ObservableList<Card> oCards = FXCollections.observableList(cards);
         view.setItems(oCards);
         view.setCellFactory(pile ->new EditCell()
         );
+        name.setText(pile.getName());
+        name.textProperty().addListener((observable, oldValue, newValue) -> {pile.setName(newValue);});
 
     }
 
