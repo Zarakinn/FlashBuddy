22d06d1ca0ddf2ad2bc32827bffccdb84e6a6276
Theo.Goureau <Theo.Goureau@telecomnancy.eu>
2023-01-04 11:14:41 +0100

    [EDIT][MODEL] Implémentation simple des piles/cartes

--- flashcards/src/main/java/tn/flashcards/model/pile/Card.java
+++ flashcards/src/main/java/tn/flashcards/model/pile/Card.java
@@ -4,4 +4,35 @@ public class Card {
     int id;
     QuestionReponse question;
     QuestionReponse reponse;
+
+    public Card(int id) {
+        this.id = id;
+        this.question = new QRText();
+        this.reponse = new QRText();
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public Card copyWithId(int id) {
+        Card c = new Card(this.id);
+        c.setQuestion(this.question);
+        c.setReponse(this.reponse);
+        return c;
+    }
+
+    public QuestionReponse getQuestion() {
+        return this.question;
+    }
+    public void setQuestion(QuestionReponse qr) {
+        this.question = qr;
+    }
+
+    public QuestionReponse getReponse() {
+        return this.reponse;
+    }
+    public void setReponse(QuestionReponse qr) {
+        this.reponse = qr;
+    }
 }
--- flashcards/src/main/java/tn/flashcards/model/pile/QRText.java
+++ flashcards/src/main/java/tn/flashcards/model/pile/QRText.java
@@ -1,5 +1,9 @@
 package tn.flashcards.model.pile;
 
 public class QRText implements QuestionReponse {
-    
+    String text;
+
+    public Object getObject() {
+        return (Object) this.text;
+    }
 }
