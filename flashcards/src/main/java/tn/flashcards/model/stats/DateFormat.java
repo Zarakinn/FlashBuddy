ff5c6e383733bfa84d5064c9c11caf9d71ac56c4
Theo.Goureau <Theo.Goureau@telecomnancy.eu>
2023-01-05 16:42:37 +0100

    [ADD] BarChart pour une pile

--- flashcards/src/main/java/tn/flashcards/model/stats/DateFormat.java
+++ flashcards/src/main/java/tn/flashcards/model/stats/DateFormat.java
@@ -6,4 +6,8 @@ public class DateFormat {
     public static DateTimeFormatter getDateTimeFormatter() {
         return DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
     }
+
+    public static DateTimeFormatter getDayFormater() {
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    }
 }
---
+++ flashcards/src/main/java/tn/flashcards/view/MyBarChart.java
@@ -0,0 +1,27 @@
+package tn.flashcards.view;
+
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+/**
+ * Prend un tableau d'entiers créé par model.stats.Calculs.tenLastDays(Pile p))
+ * comme paramètre et l'utilise pour créer un BarChart JavaFX.
+ */
+public class MyBarChart extends BarChart<String, Number> {
+    public MyBarChart(int[] donnees) {
+        super(new CategoryAxis(), new NumberAxis());
+
+        String[] legends = {"9 jours", "8 jours", "7 jours", "6 jours", "5 jours", "4 jours", "3 jours", "2 jours", "Hier", "Aujourd'hui" };
+
+        this.getXAxis().setLabel("Jour");
+        this.getYAxis().setLabel("Nombre de parties");
+
+        XYChart.Series<String, Number> singleSeries = new XYChart.Series<String, Number>();
+
+        for (int i = 0; i < 10; i++) {
+            singleSeries.getData().add(new XYChart.Data<String, Number>(legends[i], donnees[i]));
+        }
+    }
+}
