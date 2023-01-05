40a10d5be5aafdfb5643f6c133a80036c76eee8a
Theo.Goureau <Theo.Goureau@telecomnancy.eu>
2023-01-05 17:09:46 +0100

    [FIX] PieChart vide.

--- flashcards/src/main/java/tn/flashcards/view/MyPieChart.java
+++ flashcards/src/main/java/tn/flashcards/view/MyPieChart.java
@@ -13,13 +13,25 @@ public class MyPieChart extends PieChart {
     public MyPieChart(int[] donnees) {
         super();
 
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
-                new PieChart.Data("Evident", donnees[0]),
-                new PieChart.Data("Facile", donnees[1]),
-                new PieChart.Data("Moyen", donnees[2]),
-                new PieChart.Data("Difficile", donnees[3]),
-                new PieChart.Data("Impossible", donnees[4]),
-                new PieChart.Data("Non jouées", donnees[5]));
+        ObservableList<PieChart.Data> pieChartData;
+
+        if (donnees[0]+donnees[1]+donnees[2]+donnees[3]+donnees[4]+donnees[5] == 0) {
+            pieChartData = FXCollections.observableArrayList(
+                new PieChart.Data("Evident", 0),
+                new PieChart.Data("Facile", 0),
+                new PieChart.Data("Moyen", 0),
+                new PieChart.Data("Difficile", 0),
+                new PieChart.Data("Impossible", 0),
+                new PieChart.Data("Non jouées", 1));
+        } else {
+            pieChartData = FXCollections.observableArrayList(
+                    new PieChart.Data("Evident", donnees[0]),
+                    new PieChart.Data("Facile", donnees[1]),
+                    new PieChart.Data("Moyen", donnees[2]),
+                    new PieChart.Data("Difficile", donnees[3]),
+                    new PieChart.Data("Impossible", donnees[4]),
+                    new PieChart.Data("Non jouées", donnees[5]));
+        }
 
         this.setData(pieChartData);
         this.setLabelLineLength(10);
