9608317934a2e9eb9c6862106bb97283d8832f44
Theo.Goureau <Theo.Goureau@telecomnancy.eu>
2023-01-05 11:36:32 +0100

    [EDIT][MODEL] générateur d'id unique pour la pile

--- flashcards/src/main/java/tn/flashcards/model/pile/Pile.java
+++ flashcards/src/main/java/tn/flashcards/model/pile/Pile.java
@@ -5,13 +5,26 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class Pile {
 
-    private static int ID = 0;
+    private static String genUniqueId() {
+        int length = 32;
+        char chars[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_', '-'};
+
+        // --> 64^32 possibilités
+
+        String res = "";
+        for (int i = 0; i < length; i++) {
+            res = res + chars[new Random().nextInt(chars.length)];
+        }
+        return res;
+    }
+
     protected String name;
     protected String desc;
     protected String uniqueId;
@@ -23,15 +36,11 @@ public class Pile {
     public Pile() {
         this.name = "Nom de la pile";
         this.desc = "Description par défaut";
-        this.uniqueId = Integer.toString(ID);
-        this.creator = "";
+        this.uniqueId = Pile.genUniqueId();
+        this.creator = "Créateur";
         this.cards = new ArrayList<>();
         this.nextCardId = 0;
-        this.incrID();
-    }
-
-    public void incrID() {
-        ID++;
+        this.tags = "tag";
     }
 
     public Card createCard() {
